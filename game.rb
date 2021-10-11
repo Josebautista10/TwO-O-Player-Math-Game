41d78a48e5ab1d475684899c25a9a0b477f38071
Josebautista10 <cooldj1020@gmail.com>
2021-10-11 14:38:59 -0600

    required files to main and set up tthe players class

--- game.rb
+++ game.rb
@@ -1,2 +1,13 @@
-require './player.rb'
+require './players.rb'
 require './question.rb'
+
+class Game
+  def initialize
+    puts "What's player 1's name?"
+    p1 = geys.chomp
+    @p1 = Player.new(p1)
+    puts "What's player 2's name?"
+    p2 = gets.chomp
+    @p2 = Player.new(p2)
+  end
+end
