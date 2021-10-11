2b75bd0bc71c8841b9163a66163538896fcf1453
Josebautista10 <cooldj1020@gmail.com>
2021-10-11 17:57:24 -0600

    testing and fixing small bugs in program

--- game.rb
+++ game.rb
@@ -3,71 +3,72 @@ require './question.rb'
 
 class Game
   def initialize
-    @players = []
-    @players[0] = Players.new('player1') 
-    @players[1] = Players.new('player2')
+    @player1 = Players.new('player1') 
+    @player2 = Players.new('player2')
   end
   
     
     def opening_question
       @new_question = Question.new
       puts "---- NEW TURN ----"
-      if @players[0].player_turn
-        puts "#{@players[0].name}: #{@new_questionl.question}"
+      if @player1.player_turn
+        puts "#{@player1.name}: #{@new_question.question}"
       else 
-        puts "#{@players[1].name}: #{@new_question.question}"
+        puts "#{@player2.name}: #{@new_question.question}"
       end
     end
 
     def evaluate_answer
       if gets.chomp.to_i == @new_question.answer
-        if @player[0].player_turn
-          puts "Awesome job Player 1"
-          @player[0].player_turn = false
-          @player[1].player_turn = true
-          puts "#{player[0].name}: #{@player[0].lives}/3 vs #{player[1].name}: #{@player[1].lives}/3"
-          winner?
+        if @player1.player_turn
+            puts "Awesome job Player 1"
+            @player1.player_turn = false
+            @player2.player_turn = true
+            puts "#{@player1.name}: #{@player1.lives}/3 vs #{@player2.name}: #{@player2.lives}/3"
+            winner?
         else 
-          puts "Awesome job Player 2"
-          @player[0].player_turn = true
-          @player[1].player_turn = false
-          puts "#{player[0].name}: #{@player[0].lives}/3 vs #{player[1].name}: #{@player[1].lives}/3"
-          winner?
+            puts "Awesome job Player 2"
+            @player1.player_turn = true
+            @player2.player_turn = false
+            puts "#{@player1.name}: #{@player1.lives}/3 vs #{@player2.name}: #{@player2.lives}/3"
+            winner?
         end
       else
-        if @player[0].player_turn
-            @player[0].lives -= 1
+        if @player1.player_turn
+            @player1.lives -= 1
             puts "Not very quick maths. You lost a life"
-            @player[0].player_turn = false
-            @player[1].player_turn = true
-            puts "#{player[0].name}: #{@player[0].lives}/3 vs #{player[1].name}: #{@player[1].lives}/3"
-          winner?
+            @player1.player_turn = false
+            @player2.player_turn = true
+            puts "#{@player1.name}: #{@player1.lives}/3 vs #{@player2.name}: #{@player2.lives}/3"
+            winner?
         else 
-          @player[1].lives -= 1
+            @player2.lives -= 1
             puts "Not very quick maths. You lost a life"
-            @player[0].player_turn = true
-            @player[1].player_turn = false
-            puts "#{player[0].name}: #{@player[0].lives}/3 vs #{player[1].name}: #{@player[1].lives}/3"
-          winner?
+            @player1.player_turn = true
+            @player2.player_turn = false
+            puts "#{@player1.name}: #{@player1.lives}/3 vs #{@player2.name}: #{@player2.lives}/3"
+            winner?
         end
       end
-end
+    end
+    
+    def next_turn
+      opening_question
+      evaluate_answer
+    end
 
       def winner?
-        if @player[0].lives == 0
+        if @player1.lives == 0
           puts " ---- GAME OVER ---- "
-          puts "#{@player[1].name} wins with a score of #{@player[0].lives}/3"
+          puts "#{@player2.name} wins with a score of #{@player1.lives}/3"
           puts "Try again"
-        elsif
+        elsif @player2.lives == 0
           puts " ---- GAME OVER ---- "
-          puts "#{@player[0].name} wins with score of #{@player[0].lives}/3"
+          puts "#{@player1.name} wins with score of #{@player1.lives}/3"
           puts "Try again"
         else
           next_turn
         end 
       end
-
-      def next_turn
-        opening_question
-        evaluate_answer
-      end
+      
+end
--- main.rb
+++ main.rb
@@ -3,4 +3,4 @@ require "./question.rb"
 require "./game.rb"
 
 game = Game.new
-game.opening_question
+game.next_turn
